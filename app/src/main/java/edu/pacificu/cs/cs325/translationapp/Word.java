abe201278ac0b7851240058981eda4a92f1cf5bd
flor0357 <flor0357@pacificu.edu>
2024-11-20 13:47:15 -0800

    created Word Class

--- app/src/main/java/edu/pacificu/cs/cs325/translationapp/Word.java
+++ app/src/main/java/edu/pacificu/cs/cs325/translationapp/Word.java
@@ -1,5 +1,6 @@
 package edu.pacificu.cs.cs325.translationapp;
 
+import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
@@ -8,5 +9,68 @@ public class Word
 {
   @PrimaryKey (autoGenerate = true)
   private int mWid;
-  //testing push
+
+  @ColumnInfo(name="mcEnglishWord")
+  private String mcEnglishWord;
+
+  @ColumnInfo(name="mcTranslatedWord")
+  private String mcTranslatedWord;
+
+  @ColumnInfo(name="mcDefinition")
+  private String mcDefinition;
+
+  @ColumnInfo(name="mcLexical")
+  private String mcLexical;
+
+  @ColumnInfo(name="mcLexical")
+  private String mcLanguage;
+
+  public Word(String mcEnglishWord, String mcTranslatedWord, String mcDefinition,
+      String mcLexical, String mcLanguage)
+  {
+    this.mcEnglishWord = mcEnglishWord;
+    this.mcTranslatedWord = mcTranslatedWord;
+    this.mcDefinition = mcDefinition;
+    this.mcLexical = mcLexical;
+    this.mcLanguage = mcLanguage;
+
+  }
+
+  public String getEnglishWord()
+  {
+    return this.mcEnglishWord;
+  }
+
+  public String getTranslatedWord()
+  {
+    return this.mcTranslatedWord;
+  }
+
+  public String getDefinition()
+  {
+    return this.mcDefinition;
+  }
+
+  public String getLexical()
+  {
+    return this.mcLexical;
+  }
+
+  public String getLanguage()
+  {
+    return this.mcLanguage;
+  }
+
+  public void setWid(int wid)
+  {
+    this.mWid = wid;
+  }
+
+  public int getWid()
+  {
+    return this.mWid;
+  }
+
+
+
 }
