ca7b1a84c394c3f6ff7583168472c7525a415c60
flor0357 <flor0357@pacificu.edu>
2024-11-20 15:21:12 -0800

    finished? Dictionary

--- app/src/main/java/edu/pacificu/cs/cs325/translationapp/Vocab.java
+++ app/src/main/java/edu/pacificu/cs/cs325/translationapp/Vocab.java
@@ -2,4 +2,24 @@ package edu.pacificu.cs.cs325.translationapp;
 
 public class Vocab
 {
+  private Word mcTheWord;
+
+  private Byte[] mcImage;
+
+  public Vocab(Word mcTheWord, Byte[] mcImage)
+  {
+    this.mcTheWord = mcTheWord;
+    this.mcImage = mcImage;
+  }
+
+  public Byte[] getImage()
+  {
+    return mcImage;
+  }
+
+  public Word getWord()
+  {
+    return this.mcTheWord;
+  }
+
 }
