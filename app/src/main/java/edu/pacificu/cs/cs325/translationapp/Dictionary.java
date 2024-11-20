e464a0cad41f5f45667a7b4882aea8d74c30d132
flor0357 <flor0357@pacificu.edu>
2024-11-20 13:59:08 -0800

    finished? Dictionary

--- app/src/main/java/edu/pacificu/cs/cs325/translationapp/Dictionary.java
+++ app/src/main/java/edu/pacificu/cs/cs325/translationapp/Dictionary.java
@@ -1,5 +1,38 @@
 package edu.pacificu.cs.cs325.translationapp;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Dictionary
 {
+  private HashMap<String, Word> mcDictionary;
+
+  private String mcLanguage;
+
+  public Dictionary(String cLanguage)
+  {
+    mcDictionary = new HashMap<> ();
+    mcLanguage = cLanguage;
+  }
+
+  public void insertWord(String theString, Word theWord)
+  {
+    mcDictionary.put(theString, theWord);
+  }
+
+  public Word getWord(String theWord)
+  {
+    return mcDictionary.get(theWord);
+  }
+
+//  public ArrayList<Word> getAllWords()
+//  {
+//    ArrayList listOfWords = ArrayList<>(mcDictionary.values());
+//    return mcDictionary.values ();
+//  }
+  public String getLanguage()
+  {
+    return this.mcLanguage;
+  }
+
 }
