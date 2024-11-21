978cc9c0e3f20621c3544528641ef51f7d916b71
Jason Tran <tran3066@pacific.edu>
2024-11-21 15:09:23 -0800

    Started implementing IDictionaryReader and TXTDictionaryReader (need to upload the cleaned oxford dictionary online somewhere)

--- app/src/main/java/edu/pacificu/cs/cs325/translationapp/IDictionaryReader.java
+++ app/src/main/java/edu/pacificu/cs/cs325/translationapp/IDictionaryReader.java
@@ -1,5 +1,46 @@
 package edu.pacificu.cs.cs325.translationapp;
 
+import java.io.InputStream;
+
+/**
+ * Creates an abstract IDictionaryReader class that stores an InputStream object
+ * that determines where the dictionary will read
+ *
+ * @author Jason Tran
+ */
+
 public abstract class IDictionaryReader
 {
+  private final InputStream mcInputStream;
+
+  /**
+   * Initializes IDictionaryReader using parameter list values
+   *
+   * @param cInputStream input stream from which the dictionary will read
+   */
+
+  public IDictionaryReader (InputStream cInputStream)
+  {
+    mcInputStream = cInputStream;
+  }
+
+  /**
+   * Pure virtual method that is intended to read from the input stream
+   *
+   * @param cDictionary dictionary containing the Oxford dictionary
+   * @return true if the input stream was successfully read from; else, false
+   */
+
+  public abstract boolean read (Dictionary cDictionary);
+
+  /**
+   * Obtain the input stream
+   *
+   * @return input stream (mcInputStream)
+   */
+
+  protected InputStream getInputStream ()
+  {
+    return mcInputStream;
+  }
 }
--- app/src/main/java/edu/pacificu/cs/cs325/translationapp/TXTDictionaryReader.java
+++ app/src/main/java/edu/pacificu/cs/cs325/translationapp/TXTDictionaryReader.java
@@ -1,5 +1,66 @@
 package edu.pacificu.cs.cs325.translationapp;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Creates a TXTDictionaryReader that implements the methods in the abstract
+ * class IDictionaryReader. As implied by its name, TXTDictionaryReader reads
+ * from a txt file
+ *
+ * @author Jason Tran
+ */
+
 public class TXTDictionaryReader extends IDictionaryReader
 {
+  /**
+   * Initializes TXTDictionaryReader using parameter list values
+   *
+   * @param cInputStream input stream from which the dictionary will read
+   */
+
+  public TXTDictionaryReader (InputStream cInputStream)
+  {
+    super (cInputStream);
+  }
+
+  /**
+   * Reads from a text file
+   *
+   * @param cDictionary dictionary to be initialized
+   * @return true if the text file was successfully read from; else, false
+   */
+
+  @Override
+  public boolean read (Dictionary cDictionary)
+  {
+    final int AFTER_FIRST_WHITESPACE = 2;
+
+    boolean bRetVal = true;
+
+    try
+    {
+      try (BufferedReader cBR = new BufferedReader (
+          new InputStreamReader (this.getInputStream ())))
+      {
+        String cLine;
+
+        while ((cLine = cBR.readLine ()) != null)
+        {
+          // do stuff
+        }
+      }
+    }
+    catch (IOException cError)
+    {
+      bRetVal = false;
+      System.out.println ("Error reading dictionary: " + cError);
+    }
+
+    return bRetVal;
+  }
 }
