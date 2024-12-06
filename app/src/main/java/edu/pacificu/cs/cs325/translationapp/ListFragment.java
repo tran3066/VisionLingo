f0abe6fc9a2d1fc62a56910eaf0780564aa1eb5b
tran3066 <tran3066@pacificu.edu>
2024-12-06 12:27:54 -0800

    Working on converting list activity to fragment

--- app/src/main/java/edu/pacificu/cs/cs325/translationapp/ListFragment.java
+++ app/src/main/java/edu/pacificu/cs/cs325/translationapp/ListFragment.java
@@ -8,6 +8,9 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,48 +18,81 @@ import android.view.ViewGroup;
 
 import edu.pacificu.cs.cs325.translationapp.databinding.FragmentListBinding;
 
-public class ListFragment extends Fragment {
+/**
+ * Creates a ListFragment class that contains a vocabulary list containing words
+ * obtained from the camera screen or from the word the user searched on
+ * InfoFragment. This screen is similar to InfoFragment, except it provides a
+ * history of all the words detected and added, displayed in a RecyclerView.
+ * This vocabulary list will be used as a way for the user to refer back to the
+ * words they have already scanned. The words in the vocabulary list will be
+ * used in the vocabulary quiz in QuizFragment. If the user wants to add more
+ * words they can use the navigation bar to take them to either CameraFragment,
+ * the camera, to take picture of objects for the words, or to InfoFragment,
+ * to search up more words for their vocabulary list.
+ *
+ * @author Jason Tran
+ */
 
-     FragmentListBinding mcBinding;
+public class ListFragment extends Fragment
+{
+  private FragmentListBinding mcBinding;
+  private VocabRecyclerViewAdapter mcAdapter;
 
+  public ListFragment ()
+  {
+    // Required empty public constructor
+  }
 
-    public ListFragment ()
-    {
-        // Required empty public constructor
-    }
+  @Override
+  public View onCreateView (@NonNull LayoutInflater inflater,
+      ViewGroup container, Bundle savedInstanceState)
+  {
+    mcBinding = edu.pacificu.cs.cs325.translationapp.databinding.FragmentListBinding.inflate (
+        getLayoutInflater ());
+    return mcBinding.getRoot ();
+  }
 
-    @Override
-    public View onCreateView (@NonNull LayoutInflater inflater, ViewGroup container,
-                              Bundle savedInstanceState)
-    {
-        mcBinding = edu.pacificu.cs.cs325.translationapp.databinding.FragmentListBinding.inflate (
-                getLayoutInflater ());
-        return mcBinding.getRoot ();
-    }
+  @Override
+  public void onViewCreated (@NonNull View view,
+      @Nullable Bundle savedInstanceState)
+  {
+    super.onViewCreated (view, savedInstanceState);
+    BusinessLogic mcLogic = new ViewModelProvider (this).get (
+        BusinessLogic.class);
 
-    @Override
-    public void onViewCreated (@NonNull View view,
-                               @Nullable Bundle savedInstanceState)
-    {
-        super.onViewCreated (view, savedInstanceState);
-        BusinessLogic mcLogic = new ViewModelProvider(this).get(BusinessLogic.class);
+    assert getActivity () != null;
+    getActivity ().findViewById (android.R.id.content)
+        .setBackgroundResource (mcColor);
 
-        assert getActivity() != null;
-        getActivity().findViewById(android.R.id.content).setBackgroundResource(mcColor);
+    mcBinding.rvWords.setHasFixedSize (true);
+    mcBinding.rvWords.setLayoutManager (
+        new LinearLayoutManager (getActivity ()));
+  }
 
-    }
+  @Override
+  public void onCreate (Bundle savedInstanceState)
+  {
+    super.onCreate (savedInstanceState);
 
-    @Override
-    public void onCreate (Bundle savedInstanceState)
-    {
-        super.onCreate (savedInstanceState);
+  }
 
-    }
+  @Override
+  public void onDestroyView ()
+  {
+    super.onDestroyView ();
+  }
 
-    @Override
-    public void onDestroyView ()
-    {
-        super.onDestroyView ();
-    }
+  /**
+   * onResume method that reinitializes VocabRecyclerViewAdapter and sets it
+   * to the RecyclerView every time the fragment becomes visible to the user
+   */
 
+  @Override
+  public void onResume ()
+  {
+    super.onResume ();
+    mcAdapter = new VocabRecyclerViewAdapter (
+        HomeActivity.mcCurrentUser.getMcVocabList ());
+    mcBinding.rvWords.setAdapter (mcAdapter);
+  }
 }
